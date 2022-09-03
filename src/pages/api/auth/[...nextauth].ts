d98addfef735a1dd5fd880b6157195bdeb44856a
marco <rezende@espressolabs.com.br>
2022-09-03 01:30:03 -0300

    update

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -26,7 +26,7 @@ export default NextAuth({
      * on production, it's recommended to use the node-jose-tools to
      * generate one.
      */
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET || 'default',
   },
   /**
    * after login, there are some callback that are called, and among them we
